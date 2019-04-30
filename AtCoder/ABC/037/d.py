475058c3280b1a881467376ef032f57df062bbe5
tatsu <tatsu100311@gmail.com>
2019-05-01 02:01:36 +0900

    Add ABC037 D

--- AtCoder/ABC/037/d.py
+++ AtCoder/ABC/037/d.py
@@ -1,49 +1,35 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(3000)
 
 
 def f(A, i, j, memo):
-    if memo[i][j] != -1:
-        return memo
+    if memo[i][j] != 0:
+        return memo[i][j]
 
-    if A[i][j] >= max(A[i-1][j], A[i+1][j], A[i][j-1], A[i][j+1]):
-        memo[i][j] = 1
-        return memo
+    res = 1
+    H, W = len(A), len(A[0])
+    for s, t in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+        if 0 <= i+s < H and 0 <= j+t < W and A[i][j] < A[i+s][j+t]:
+            res += f(A, i+s, j+t, memo)
 
-    ans = 1
-    for s, t in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        if A[i][j] < A[i+s][j+t]:
-            memo = f(A, i+s, j+t, memo)
-            ans += memo[i+s][j+t]
-
-    memo[i][j] = ans
-    return memo
+    res %= 10**9 + 7
+    memo[i][j] = res
+    return res 
 
 
 def main():
     input = sys.stdin.readline
     H, W = map(int, input().split())
-    A = []
-    A.append([0 for _ in range(W+2)])
-    for _ in range(H):
-        a = [0] + list(map(int, input().split())) + [0]
-        A.append(a)
-    A.append([0 for _ in range(W+2)])
+    A = [list(map(int, input().split())) for _ in range(H)]
+
+    memo = [[0 for _ in range(W)] for _ in range(H)]
+    for i in range(H):
+        for j in range(W):
+            if memo[i][j] == 0:
+                f(A, i, j, memo)
 
     MOD = 10**9 + 7
-    memo = [[-1 for _ in range(W+2)] for _ in range(H+2)]
-
-    ans = 0
-    for i in range(1, H+1):
-        for j in range(1, W+1):
-            if memo[i][j] != -1:
-                ans += memo[i][j]
-                ans %= MOD
-            else:
-                memo = f(A, i, j, memo)
-                ans += memo[i][j]
-                ans %= MOD
-
+    ans = sum(sum(m) % MOD for m in memo)
     return ans % MOD
 
 
