a2d29de00d32502ca5bc9804727abbbefae6a0c3
tatsu <tatsu100311@gmail.com>
2019-05-09 12:28:44 +0900

    Add AtCoder ABC024 D

--- AtCoder/ABC/024/d.py
+++ AtCoder/ABC/024/d.py
@@ -1,14 +1,44 @@
-import numpy as np
 import sys
+
+
+def get_inverse(n, mod):
+    i = mod - 2
+    res = 1
+    while i > 0:
+        if i % 2 == 1:
+            res *= n
+            res %= mod
+
+        n **= 2
+        n %= mod
+        i //= 2
+
+    return res
+
 
 def main():
     input = sys.stdin.readline
-    N = int(input())
-    N, K = map(int, input().split())
-    A = list(map(int, input().split()))
+    MOD = 10**9 + 7
+    A = int(input())
+    B = int(input())
+    C = int(input())
 
-    return ans
+    # r = (BC - CA) / (AB - BC + CA)
+    # c = (BC - AB) / (AB - BC + CA)
+
+    # According to Fermat's little theorem,
+    # a^(-1) = a^(p-2) (mod p)
+
+    r_numerator = (B*C - C*A) % MOD
+    c_numerator = (B*C - A*B) % MOD
+    denominator = A*B - B*C + C*A
+    den_inverse = get_inverse(denominator, MOD)
+
+    r = (r_numerator * den_inverse) % MOD
+    c = (c_numerator * den_inverse) % MOD
+
+    return r, c
 
 
 if __name__ == '__main__':
-    print(main())
+    print(*main())
