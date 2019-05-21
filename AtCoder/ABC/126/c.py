ae2b99f7cd72e05c771372f1f40c6a0dcd755c9c
tatsu <tatsu100311@gmail.com>
2019-05-21 19:57:49 +0900

    Update AtCoder ABC126 C

--- AtCoder/ABC/126/c.py
+++ AtCoder/ABC/126/c.py
@@ -6,14 +6,11 @@ def main():
 
     ans = 0
     for i in range(1, N+1):
-        if i >= K:
-            ans += 1 / N
-        else:
-            cnt = 1
-            while i * 2**cnt < K:
-                cnt += 1
+        cnt = 0
+        while i * 2**cnt < K:
+            cnt += 1
 
-            ans += 1 / N * (0.5)**cnt
+        ans += 1 / N * (0.5)**cnt
 
     return ans
 
