4fc7cc60cdec9ae0c9e01522981b916c65792b32
tatsu <tatsu100311@gmail.com>
2019-05-30 20:15:07 +0900

    [WIP] AtCoder ABC128 F

---
+++ AtCoder/ABC/128/f.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+    N = int(input())
+    S = list(map(int, input().split()))
+
+    return ans
+
+
+if __name__ == '__main__':
+    print(main())
