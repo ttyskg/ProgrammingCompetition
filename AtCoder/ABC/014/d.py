cae4da434ac08613dc388ee2ff1cee1938e055d6
tatsu <tatsu100311@gmail.com>
2019-06-16 23:23:40 +0900

    Modify AtCoder ABC014 D

--- AtCoder/ABC/014/d.py
+++ AtCoder/ABC/014/d.py
@@ -5,37 +5,42 @@ sys.setrecursionlimit(10**6)
 def main():
     input = sys.stdin.readline
     N = int(input())
-    d = defaultdict(list)
+    edges  = defaultdict(list)
     for _ in range(N-1):
         x, y = map(int, input().split())
-        d[x-1].append(y-1)
-        d[y-1].append(x-1)
+        edges[x-1].append(y-1)
+        edges[y-1].append(x-1)
 
     # Make a list of the tree structures.
-    # tree[i] = (depth, parent)
-    tree  = [0] * N  # parent, depth
-    q = deque()
-    q.append((-1, 0))
-    visited = [False] * N
-    depth  = 0
-    while len(q) > 0:
-        q_ = deque()
-        while len(q) > 0:
-            parent, child =  q.popleft()
-            tree[child] = (depth, parent)
-            visited[child] = True
-            for gc in d[child]:
-                if visited[gc]:
-                    continue
-                q_.append((child, gc))
-        q = q_
-        depth  += 1
+    # parents[i] is parent of i-th node. The 0-th node is root.
+    # depth[i] is depth of i-th node from 0-th node.
+    parents = [0] * N
+    depths = [0] * N
+    def make_tree(parent, node):
+        for child in edges[node]:
+            if child == parent:
+                continue
+            parents[child] = node
+            depths[child] = depths[node] + 1
+            make_tree(node, child)
+    make_tree(0, 0)
 
-    def dfs(n):
-        depth, parent = tree[n]
-        if parent in doubling:
-            doubling[n] =
+    N2 = len(bin(N)) - 2
+    doubling = [[0] * N2 for _ in range(N)]
+    def make_doubling(node, parent):
+        doubling[node][0] = parent
+        for i in range(1, N2):
+            doubling[node][i] = doubling[doubling[node][i-1]][i-1]
+            if doubling[node][i] == 0:
+                break
+        for n in edges[node]:
+            if n == parent:
+                continue
+            make_doubling(n, node)
+    make_doubling(0, 0)
 
+    print(doubling)
+    return 0
 
     doubling = dict()
     for child in d[0]:
