b2d2fd6ffec382df287ea3b693197c93614f4c8d
tatsu <tatsu100311@gmail.com>
2019-06-21 01:10:16 +0900

    Add AtCoder ABC130 E

--- AtCoder/ABC/130/e.py
+++ AtCoder/ABC/130/e.py
@@ -4,28 +4,18 @@ def main():
     input = sys.stdin.readline
     MOD = 10**9 + 7
     N, M = map(int, input().split())
-    S = list(map(int, input().split()))
-    T = list(map(int, input().split()))
+    S = [0] + list(map(int, input().split()))
+    T = [0] + list(map(int, input().split()))
 
-    n = min(N, M)
-    ans = 1
-    for i in range(1, n+1):
-        ds = defaultdict(lambda: 0)
-        dt = defaultdict(lambda: 0)
+    dp = [[0] * (M+1) for _ in range(N+1)]
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
+            if S[i] == T[j]:
+                dp[i][j] += dp[i-1][j-1] + 1
+            dp[i][j] %= MOD
 
-        for left in range(N - i + 1):
-            key = tuple(S[left:left+i])
-            ds[key] += 1
-
-        for left in range(M - i + 1):
-            key = tuple(T[left:left+i])
-            dt[key] += 1
-
-        for key in ds.keys():
-            ans += ds[key] * dt[key]
-            ans %= MOD
-
-    return ans
+    return (dp[N][M] + 1) % MOD
 
 
 if __name__ == '__main__':
