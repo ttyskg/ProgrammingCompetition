43b65600ebabbcb07d53a048918c1d55601af530
tatsu <tatsu100311@gmail.com>
2019-06-24 00:01:31 +0900

    [WIP] AtCoder ABC131 F

---
+++ AtCoder/ABC/131/f.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def main():
+    input = sys.stdin.readline
+    N = int(input())
+    x_exist = set()
+    y_exist = set()
+    cnt_x, cnt_y = 0, 0
+    for _ in range(N):
+        x, y = map(int, input().split())
+
+
+    return ans
+
+
+if __name__ == '__main__':
+    print(main())
