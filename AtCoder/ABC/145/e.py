ee8c6dceefda1b576b76a3ccc5db82ca23cb7c55
tatsu <tatsu100311@gmail.com>
2019-11-16 22:35:50 -0500

    Add AtCoder ABC145 E

--- AtCoder/ABC/145/e.py
+++ AtCoder/ABC/145/e.py
@@ -1,12 +1,21 @@
 import sys
-sys.setrecursionlimit(10**6)
+import numpy as np
 
 def main():
     input = sys.stdin.readline
-    N = int(input())
-    S = str(input().strip())
-    N, K = map(int, input().split())
-    A = list(map(int, input().split()))
+    N, T = map(int, input().split())
+    dishes = []
+    for _ in range(N):
+        a, b = map(int, input().split())
+        dishes.append((a, b))
+    
+    dishes = sorted(dishes, key=lambda x: x[0])
+    
+    ans = 0
+    dp = np.zeros(T, dtype=int)
+    for a, b in dishes:
+        ans = max(ans, dp.max() + b)
+        np.maximum(dp[:-a] + b, dp[a:], out=dp[a:])
 
     return ans
 
